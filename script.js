2a0aec081293cc08cffbc030d59d4603f469a5b1
Tairah <Tairah.akintoye@ocsbstudent.ca>
2021-03-17 20:41:23 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('button').addEventListener('click', displayMessage)
+
+function displayMessage () {
+  document.getElementById('paragraph').innerHTML = 'How are you doing today,make sure to enjoy yourself and stay safe.'
+}
